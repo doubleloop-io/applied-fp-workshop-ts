572fc3234206554661ab4a0f7bfc237b19ada3c4
Matteo Baglini <matteo.baglini@gmail.com>
2022-12-22 11:41:41 +0100

    combination phase effectful

---
+++ src/exercises/combination-phase-effectful.test.ts
@@ -0,0 +1,42 @@
+import { Option } from "fp-ts/Option"
+import * as O from "fp-ts/Option"
+import { pipe } from "fp-ts/lib/function"
+
+describe.skip("combination phase", () => {
+    type Item = {
+        qty: number
+    }
+
+    type createItemFn = (qty: string) => Option<Item>
+    const createItem: createItemFn = (qty) =>
+        qty.match(/^[0-9]+$/i) ? O.some({ qty: parseInt(qty, 10) }) : O.none
+
+    type checkInFn = (value: number) => (item: Item) => Item
+    const checkIn: checkInFn = (value) => (item) => ({ qty: item.qty + value })
+
+    type checkOutFn = (value: number) => (item: Item) => Option<Item>
+    const checkOut: checkOutFn = (value) => (item) =>
+        value <= item.qty ? O.some({ qty: item.qty - value }) : O.none
+
+    test("checkOut after valid creation", () => {
+        const result = pipe(createItem("100"), O.chain(checkOut(10)))
+
+        expect(result).toStrictEqual(O.some({ qty: 90 }))
+    })
+
+    test("checkOut after invalid creation", () => {
+        const result = pipe(createItem("asd"), O.chain(checkOut(10)))
+
+        expect(result).toStrictEqual(O.none)
+    })
+
+    test("checkIn and checkOut after valid creation", () => {
+        const result = pipe(
+            createItem("100"),
+            O.map(checkIn(10)),
+            O.chain(checkOut(20)),
+        )
+
+        expect(result).toStrictEqual(O.some({ qty: 90 }))
+    })
+})
---
+++ src/exercises/solutions/combination-phase-effectful.test.ts
@@ -0,0 +1,52 @@
+import { Option } from "fp-ts/Option"
+import * as O from "fp-ts/Option"
+import { pipe } from "fp-ts/lib/function"
+
+// TODO - 1: remove skip marker
+describe.skip("combination phase", () => {
+    type Item = {
+        qty: number
+    }
+
+    type createItemFn = (qty: string) => Option<Item>
+    const createItem: createItemFn = (qty) =>
+        qty.match(/^[0-9]+$/i) ? O.some({ qty: parseInt(qty, 10) }) : O.none
+
+    type checkInFn = (value: number) => (item: Item) => Item
+    const checkIn: checkInFn = (value) => (item) => ({ qty: item.qty + value })
+
+    type checkOutFn = (value: number) => (item: Item) => Option<Item>
+    const checkOut: checkOutFn = (value) => (item) =>
+        value <= item.qty ? O.some({ qty: item.qty - value }) : O.none
+
+    test("checkOut after valid creation", () => {
+        const result = pipe(
+            createItem("100"),
+            // TODO - 2: "call" checkOut 10
+        )
+
+        // TODO - 3: change expectation
+        expect(result).toStrictEqual(null)
+    })
+
+    test("checkOut after invalid creation", () => {
+        const result = pipe(
+            createItem("asd"),
+            // TODO - 4: "call" checkOut 10
+        )
+
+        // TODO - 5: change expectation
+        expect(result).toStrictEqual(null)
+    })
+
+    test("checkIn and checkOut after valid creation", () => {
+        const result = pipe(
+            createItem("100"),
+            // TODO - 6: "call" checkIn 10
+            // TODO - 7: "call" checkOut 20
+        )
+
+        // TODO - 8: change expectation
+        expect(result).toStrictEqual(null)
+    })
+})
