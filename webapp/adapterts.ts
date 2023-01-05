41a0540283b42fcf2eed6d820b3b6cf8b3e41417
Matteo Baglini <matteo.baglini@gmail.com>
2023-01-05 19:19:50 +0100

    use request as command source

--- webapp/adapterts.ts
+++ webapp/adapterts.ts
@@ -1,14 +1,21 @@
 // ADAPTERS
 
 import { CommandsChannel, MissionReport, MissionSource } from "./ports"
+import { TaskEither } from "fp-ts/TaskEither"
+import { flow, pipe } from "fp-ts/function"
+import { loadTuple } from "../app/utils/infra-file"
+import * as TE from "fp-ts/TaskEither"
+import * as E from "fp-ts/Either"
+import { logError, logInfo } from "../app/utils/infra-console"
+import { Task } from "fp-ts/Task"
+import { Planet, Rover, ParseError } from "./core"
 import {
-  loadPlanet,
-  loadRover,
-  loadCommands,
-  writeSequenceCompleted,
-  writeObstacleDetected,
-  writeError,
-} from "./infrastructure"
+  renderComplete,
+  renderError,
+  renderObstacle,
+  renderParseError,
+} from "./rendering"
+import { parsePlanet, parseRover, parseCommands } from "./parser"
 
 export const createFileMissionSource = (
   pathPlanet: string,
@@ -17,11 +24,38 @@ export const createFileMissionSource = (
   readPlanet: () => loadPlanet(pathPlanet),
   readRover: () => loadRover(pathRover),
 })
-export const createStdinCommandsChannel = (): CommandsChannel => ({
-  read: loadCommands,
+
+export const createRequestCommandsChannel = (params: {
+  commands: string
+}): CommandsChannel => ({
+  read: () =>
+    pipe(params.commands, parseCommands, E.mapLeft(toError), TE.fromEither),
 })
 export const createStdoutMissionReport = (): MissionReport => ({
   sequenceCompleted: writeSequenceCompleted,
   obstacleDetected: writeObstacleDetected,
   missionFailed: writeError,
 })
+
+const toError = (error: ParseError): Error => new Error(renderParseError(error))
+
+export const loadPlanet = (path: string): TaskEither<Error, Planet> =>
+  pipe(
+    loadTuple(path),
+    TE.chain(flow(parsePlanet, E.mapLeft(toError), TE.fromEither)),
+  )
+
+export const loadRover = (path: string): TaskEither<Error, Rover> =>
+  pipe(
+    loadTuple(path),
+    TE.chain(flow(parseRover, E.mapLeft(toError), TE.fromEither)),
+  )
+
+export const writeSequenceCompleted = (rover: Rover): Task<void> =>
+  pipe(renderComplete(rover), logInfo)
+
+export const writeObstacleDetected = (rover: Rover): Task<void> =>
+  pipe(renderObstacle(rover), logInfo)
+
+export const writeError = (error: Error): Task<void> =>
+  pipe(renderError(error), logError)
--- webapp/index.ts
+++ webapp/index.ts
@@ -1,26 +1,37 @@
 import Fastify from "fastify"
-import { Task } from "fp-ts/Task"
 import {
   createFileMissionSource,
-  createStdinCommandsChannel,
+  createRequestCommandsChannel,
   createStdoutMissionReport,
 } from "./adapterts"
 import { runApp } from "./handler"
-
-const runAppWired = (pathPlanet: string, pathRover: string): Task<void> =>
-  runApp(
-    createFileMissionSource(pathPlanet, pathRover),
-    createStdinCommandsChannel(),
-    createStdoutMissionReport(),
-  )
 
 const fastify = Fastify({
   logger: true,
 })
 
-fastify.get("/", async (request, reply) => {
-  const app = runAppWired("data/planet.txt", "data/rover.txt")
+const pathPlanet = "data/planet.txt"
+const pathRover = "data/rover.txt"
+
+interface RoverParams {
+  commands: string
+}
+
+fastify.get<{
+  Params: RoverParams
+}>("/rover/:commands", async (request, reply) => {
+  const missionSource = createFileMissionSource(pathPlanet, pathRover)
+  const commandsSource = createRequestCommandsChannel(request.params)
+  // TODO: mission report from reply
+  const missionReport = createStdoutMissionReport()
+
+  const app = runApp(missionSource, commandsSource, missionReport)
   await app()
+  reply.type("application/json").code(200)
+  return { hello: "world" }
+})
+
+fastify.get("/", async (request, reply) => {
   reply.type("application/json").code(200)
   return { hello: "world" }
 })
@@ -30,5 +41,5 @@ fastify.listen({ port: 8080 }, (err, address) => {
     console.error(err)
     process.exit(1)
   }
-  console.log(`Server listening at ${address}`)
+  console.log(`Server listening at ${address}/rover/RFF`)
 })
