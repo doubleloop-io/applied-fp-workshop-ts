0819c6de8ac3cc97725beefe02ae64d8a663ac07
Matteo Baglini <matteo.baglini@gmail.com>
2023-11-16 14:22:34 +0100

    chore: review model data with types

--- demo/01-types.test.ts
+++ demo/01-types.test.ts
@@ -12,32 +12,37 @@ describe("model data - algebraic data types", () => {
   type Name = string
 
   // Type wrapper - rename a type and change it
-  type Age = { value: number }
+  type Age = Readonly<{ value: number }>
+  // type Age = { readonly value: number }
+
+  // value constructor
   const age = (value: number): Age => ({ value })
-  // alternative implementation with native class
+
+  // alternative implementation with class
   // class Age {
   //   constructor(readonly value: number) {}
   // }
 
   it("type wrapper - values equality", () => {
     const value = age(12)
+    // alternative creation with class
     // const value = new Age(12)
     expect(value).toStrictEqual(value)
 
     const same = age(12)
-    // const same = new Age(12)
     expect(value).toStrictEqual(same)
 
     const different = age(20)
-    // const different = new Age(20)
     expect(value).not.toStrictEqual(different)
   })
 
-  // Composed Types Modelling
+  // Composed Types
 
-  // Product types - put many types together. e.g. struct in C, POJO in JAVA, POCO in C#.
+  // Product types - put many types together e.g. struct in C, POJO in JAVA, POCO in C#.
   // Useful to model independent data in AND e.g. a Person is composed by a name *and* an age.
-  type Person = { readonly name: string; readonly age: number }
+
+  type Person = Readonly<{ name: string; age: number }>
+  // type Person = { readonly name: string; readonly age: number }
   const person = (name: string, age: number): Person => ({ name, age })
 
   it("product type - values equality", () => {
@@ -53,25 +58,29 @@ describe("model data - algebraic data types", () => {
 
   // Sum types - model exclusive types e.g. union in C, enum in JAVA/C#.
   // Useful to model dependant data in OR e.g. the Light is on *or* off.
+
   type LightState = On | Off
-  type On = { readonly _tag: "On"; readonly intensity: number }
-  type Off = { readonly _tag: "Off" }
-  const onState = (intensity: number): LightState => ({ _tag: "On", intensity })
-  const offState = (): LightState => ({ _tag: "Off" })
+  type On = Readonly<{ _tag: "On"; intensity: number }>
+  type Off = Readonly<{ _tag: "Off" }>
+
+  const on = (intensity: number): LightState => ({ _tag: "On", intensity })
+  const off = (): LightState => ({ _tag: "Off" })
 
   it("sum type - values equality", () => {
-    const value: LightState = onState(10)
+    const value: LightState = on(10)
     expect(value).toStrictEqual(value)
 
-    const same: LightState = onState(10)
+    const same: LightState = on(10)
     expect(value).toStrictEqual(same)
 
-    const different: LightState = offState()
+    const different: LightState = off()
     expect(value).not.toStrictEqual(different)
   })
 
   // Polymorphic Types
   // Types with generics (Triple) are called: type constructor
-  // While the generics (A, B, C) are called: type parameters
-  type Triple<A, B, C> = { readonly a: A; readonly b: B; readonly c: C }
+  // While the generics labels (A, B, C) are called: type parameters
+  type Triple<A, B, C> = Readonly<{ a: A; b: B; c: C }>
+
+  const triple = <A, B, C>(a: A, b: B, c: C): Triple<A, B, C> => ({ a, b, c })
 })
